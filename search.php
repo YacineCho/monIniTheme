7ad03b1d096684f6ef0f0fe692b98a9814910891
Yacine2 <yacine.chouaita@edu.univ-fcomte.fr>
2021-10-20 16:58:34 +0200

    ajout des templates singular
    single.php et page.php

--- search.php
+++ search.php
@@ -1,1 +1,14 @@
 <?php
+get_header();
+?>
+<div class="category">
+    <h1>Champ de recherche : <?php print(get_search_query()); ?></h1>
+    <?php get_template_part('loop'); ?>
+
+</div>
+<?php
+get_sidebar();
+get_footer();
+?>
+
+
